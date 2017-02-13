56a1a801984bb6fac5d82bceb41897a107960cbf
Brian Scaturro <scaturrob@gmail.com>
2017-02-13 13:11:08 -0500

    Fetch repositories by org

--- src/psssst/github.clj
+++ src/psssst/github.clj
@@ -3,16 +3,25 @@
             [mount.core :refer [defstate]]
             [clojure.core.async :refer [chan close!]]))
 
+(defonce api-url "https://api.github.com")
+
 (defrecord Repo [owner repo])
 
 (defn pull-request-url
   "Given an Oauth token and a Repo record, create a url to request
    pull requests for the given repository"
   [token repo]
-  (-> "https://api.github.com/repos/"
-    (str (:owner repo) "/" (:repo repo) "/pulls")
+  (-> (str api-url "/repos/")
+      (str (:owner repo) "/" (:repo repo) "/pulls")
       (str "?access_token=" token)
       (str "&state=open")))
+
+(defn repo-url
+  "Given an Oauth token and an org name - get a url to
+   to request repositories for the organization"
+  [token org]
+  (-> (str api-url "/orgs/" org "/repos")
+      (str "?access_token=" token)))
 
 (defn list-pull-requests
   "Given an Oauth token and a Repo record, fetch a set
@@ -29,19 +38,35 @@
       (assoc :assignee (get-in raw-pr [:assignee :login]))
       (assoc :user (get-in raw-pr [:user :login]))))
 
+(defn repo->Repo
+  "Convert a github repository record into a Repo record"
+  [raw-repo]
+  (-> {}
+      (assoc :owner (get-in raw-repo [:owner :login]))
+      (assoc :repo (get raw-repo :name))
+      map->Repo))
+
 (defn needs-review?
   "Returns true if the pull request has not been assigned to a user. False otherwise"
   [pr]
   (nil? (:assignee pr)))
 
+(defn list-repos
+  "List all repositories for the given organization"
+  [token org]
+  (->> (client/get (repo-url token org) {:as :json})
+       :body
+       (map repo->Repo)))
+
 (defn fetch-pull-requests
   "Given a collection of Repo records and an Oauth token, fetch all open pull requests
    that have not been assigned to someone"
-  [repos token]
-  (->> (pmap (partial list-pull-requests token) repos)
-       (mapcat :body)
-       (map pr->map)
-       (filter needs-review?)))
+  [org token]
+  (let [repos (list-repos token org)]
+    (->> (pmap (partial list-pull-requests token) repos)
+         (mapcat :body)
+         (map pr->map)
+         (filter needs-review?))))
 
 (defstate github :start (chan)
                  :stop (close! chan))
